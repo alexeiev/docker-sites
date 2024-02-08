537e3f28a5f192cb7df80efbf839b2afbae05144
Alexeiev Araujo <ceievfa@gmail.com>
2024-02-08 21:16:42 +0000

    Add modify scripts

--- .github/scripts/test_vars.sh
+++ .github/scripts/test_vars.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo "variables to docker"
-echo -e "user: $TRAEFIK_USER_DASH\n
-pwd: $TRAEFIK_PWD_DASH
-email: $TRAEFIK_MAIL" > ../../site/info.txt
+touch ../../site/info.txt
+echo "user: $TRAEFIK_USER_DASH"
+echo "user: $TRAEFIK_USER_DASH" >> ../../site/info.txt
+echo "pwd: $TRAEFIK_PWD_DASH"
+echo "pwd: $TRAEFIK_PWD_DASH" >> ../../site/info.txt
+echo "email: $TRAEFIK_MAIL"
+echo "email: $TRAEFIK_MAIL"  >> ../../site/info.txt
