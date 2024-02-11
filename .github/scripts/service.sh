6dae15f524cb1e4a00bc2007b8d54975820a09be
alexeiev <ceievfa@gmail.com>
2024-02-11 00:15:53 +0000

    Modify index and script service.sh for rm old images this project

--- .github/scripts/service.sh
+++ .github/scripts/service.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+PROJ="site-static"
+
 case $1 in
     start)
-        docker compose -f /app/cicd/docker-compose.yaml -p site-static up -d
+        docker compose -f /app/cicd/docker-compose.yaml -p $PROJ up -d
         exit 0
     ;;
     stop)
-        docker compose -f /app/cicd/docker-compose.yaml -p site-static down
+        docker compose -f /app/cicd/docker-compose.yaml -p $PROJ down
+        #remover containers do site
+        docker image rm -f $(docker image ls |grep $PROJ |awk '{print $3}'|uniq)
         exit 0
     ;;
     status)
-        PSD=$(docker compose ls | grep site-static | grep running)
+        PSD=$(docker compose ls | grep $PROJ | grep running)
         if [ -n "$PSD"  ]
         then
             echo "Service is Running"
